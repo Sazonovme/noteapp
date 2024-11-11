f19b0eae1a2a516849cab3cc6b888f5ec76bd657
Maxim <mr.loraks@bk.ru>
2024-11-11 22:06:45 +0600

    refactor: some changes for test

--- internal/noteapp/server/handlers.go
+++ internal/noteapp/server/handlers.go
@@ -95,16 +95,17 @@ func (s *server) handlerAuthUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	cookie := &http.Cookie{
-		HttpOnly: true,
-		Name:     "refreshToken",
-		Value:    refSession.RefreshToken,
-	}
-	http.SetCookie(w, cookie)
+	// cookie := &http.Cookie{
+	// 	HttpOnly: true,
+	// 	Name:     "refreshToken",
+	// 	Value:    refSession.RefreshToken,
+	// }
+	// http.SetCookie(w, cookie)
 
 	m := map[string]string{
-		"accessToken": refSession.AccessToken,
-		"exp":         refSession.Exp,
+		"accessToken":  refSession.AccessToken,
+		"refreshToken": refSession.RefreshToken,
+		"exp":          refSession.Exp,
 	}
 
 	err = json.NewEncoder(w).Encode(m)
@@ -133,6 +134,56 @@ func (s *server) handlerNotes(w http.ResponseWriter, r *http.Request) {
 	err := json.NewEncoder(w).Encode(data)
 	if err != nil {
 		s.error(w, r, http.StatusInternalServerError, err)
+		return
+	}
+
+}
+
+func (s *server) handlerRefreshToken(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+
+	if r.Method != http.MethodPost {
+		fmt.Println("method OPTIONS in handler notes")
+		return
+	}
+
+	type req struct {
+		Data struct {
+			RefreshToken string `json:"refreshToken"`
+			Fingerprint  string `json:"fingerprint"`
+		} `json:"data"`
+	}
+	data := &req{}
+	err := json.NewDecoder(r.Body).Decode(data)
+	if err != nil {
+		s.error(w, r, http.StatusInternalServerError, err)
+		return
+	}
+
+	refSession, err := s.store.AuthRepository.UpdateTokens(data.Data.RefreshToken, data.Data.Fingerprint)
+	if err != nil {
+		s.error(w, r, http.StatusInternalServerError, err)
+		return
+	}
+	// cookie := &http.Cookie{
+	// 	HttpOnly: true,
+	// 	Name:     "refreshToken",
+	// 	Value:    refSession.RefreshToken,
+	// }
+	// http.SetCookie(w, cookie)
+
+	m := map[string]string{
+		"refreshToken": refSession.RefreshToken,
+		"accessToken":  refSession.AccessToken,
+		"exp":          refSession.Exp,
+	}
+
+	err = json.NewEncoder(w).Encode(m)
+	if err != nil {
+		logger.NewLog("server", "handlerCreateUser", err, m, 6, "http response: json encode error")
 		return
 	}
 
@@ -205,6 +256,7 @@ func (s *server) middlewareAuth() Middleware {
 				s.error(w, r, http.StatusUnauthorized, err)
 				return
 			}
+
 			f(w, r)
 		}
 	}
