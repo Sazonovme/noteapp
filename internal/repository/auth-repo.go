a86ff058fe886e0452051ad70f181461a6af9c0f
Maxim <mr.loraks@bk.ru>
2024-11-14 12:33:53 +0600

    refactor(auth-repo.go): rename database table

--- internal/repository/auth-repo.go
+++ internal/repository/auth-repo.go
@@ -24,16 +24,16 @@ func NewAuthRepository(db *sql.DB) *AuthRepository {
 }
 
 func (r *AuthRepository) DeleteRefreshSession(login string, fingerprint string) error {
-	_, err := r.db.Exec("DELETE FROM refreshTokens WHERE login = $1 AND fingerprint = $2", login, fingerprint)
+	_, err := r.db.Exec("DELETE FROM refreshSessions WHERE login = $1 AND fingerprint = $2", login, fingerprint)
 	return err
 }
 
 func (r *AuthRepository) WriteRefreshSession(s *RefreshSession) error {
-	_, err := r.db.Exec("INSERT INTO refreshTokens VALUES($1, $2, $3, $4, $5)", s.Login, s.RefreshToken, s.Exp, s.Iat, s.Fingerprint)
+	_, err := r.db.Exec("INSERT INTO refreshSessions VALUES($1, $2, $3, $4, $5)", s.Login, s.RefreshToken, s.Exp, s.Iat, s.Fingerprint)
 	return err
 }
 
 func (r *AuthRepository) RefreshSessionExist(rToken string, fingerprint string) error {
-	_, err := r.db.Query("SELECT login, fingerprint FROM refreshTokens WHERE refreshtoken = $1 AND fingerprint = $2", rToken, fingerprint)
+	_, err := r.db.Query("SELECT login, fingerprint FROM refreshSessions WHERE refreshtoken = $1 AND fingerprint = $2", rToken, fingerprint)
 	return err
 }
